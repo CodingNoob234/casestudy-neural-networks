12d8a20ebe8936d1b6796b0f2d67be20ad6cf16e
joeyb <joeybesseling@icloud.com>
2023-01-14 20:09:58 +0100

    added early stopping class to estimating.py

--- ml/utils/estimating.py
+++ ml/utils/estimating.py
@@ -4,7 +4,10 @@ import torch.utils
 import torch.optim as optim
 import torch.nn.functional as F
 
-def model_estimator(model, optimizer, criterion, epochs, trainloader, testloader = None, verbose = 0):
+def model_estimator(model, optimizer, criterion, epochs, trainloader, testloader=None, verbose = 0):
+    
+    earlystopper = EarlyStopper(patience = 3, min_delta = 0)
+    
     for epoch in range(1, epochs+1):
         
         running_loss = []
@@ -45,3 +48,26 @@ def model_estimator(model, optimizer, criterion, epochs, trainloader, testloader
                 running_loss += [loss.item()]
             if verbose > 0:
                 print("validation loss: {np.average(running_loss)}")
+            
+            # if validation results are not improving    
+            if earlystopper.early_stop(np.average(running_loss)):
+                print(f"early stopping due to no decrease in validation loss at epoch: {epoch}")
+            
+                
+class EarlyStopper():
+    def __init__(self, patience = 1, min_delta = 0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
+        
+    def early_stop(self, validation_loss):
+        """ check if the validation loss has not increased for {patience}-times """
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
