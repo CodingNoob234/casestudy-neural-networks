fb2299a15d2fb2b75807be5bc2e1ee1708f88057
joeyb <joeybesseling@icloud.com>
2023-01-18 12:02:36 +0100

    changes

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -5,8 +5,21 @@ class PreProcessor():
     def __init__(self, ):
         pass
     
+    
+class DataSet(Dataset):
+    """ Loads the x,y data into a Dataset instance """
+    def __init__(self, x, y):
+        self.x_t = torch.Tensor(x, dtype=torch.float32)
+        self.y_t = torch.Tensor(y, dtype=torch.float32)
+        
+    def __len__(self, ):
+        return len(self.y_t)
+    
+    def __getitem__(self, idx):
+        return self.x_train[idx], self.y_train[idx]
+    
 
-def data_to_loaders(features_train_tensor: torch.Tensor, features_test_tensor: torch.Tensor, targets_train_tensor: torch.Tensor, targets_test_tensor: torch.Tensor):
+def data_to_loaders(features_train_tensor: torch.Tensor, features_test_tensor: torch.Tensor, targets_train_tensor: torch.Tensor, targets_test_tensor: torch.Tensor) -> tuple:
     """ give training and testing features/targets as torch.Tensors, and return train/testloader"""
 
     # to data loader so torch can handle the data efficiently
